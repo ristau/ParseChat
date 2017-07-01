1d9c851bfd2410b5b15ae5f7ad2874c6be572298
Barbara Ristau <ristaub@gmail.com>
2017-06-30 22:01:29 -0700

    parse chat lab

--- app/src/main/java/com/bfr/myparseapp/ChatApplication.java
+++ app/src/main/java/com/bfr/myparseapp/ChatApplication.java
@@ -3,6 +3,7 @@ package com.bfr.myparseapp;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -17,6 +18,9 @@ public class ChatApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        ParseObject.registerSubclass(Message.class);
+
+
         // Use for monitoring Parse network traffic
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
@@ -27,9 +31,9 @@ public class ChatApplication extends Application {
         // set applicationId and server based on the values in the Heroku settings.
         // any network interceptors must be added with the Configuration Builder given this syntax
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("YOUR_APPLICATION_ID") // should correspond to APP_ID env variable
+                .applicationId("myAppId") // should correspond to APP_ID env variable
                 .clientBuilder(builder)
-                .server("https://myparseapp.herokuapp.com/parse/").build());
+                .server("http://simplechat730.herokuapp.com/parse").build());
     }
 
 
